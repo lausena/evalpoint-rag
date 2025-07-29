582353554d3266aa444a539823bcff0ea83a549d
lausena <lau.sena.gabriel@gmail.com>
2025-07-29 09:20:03 -0400

    adding helper methods and separation

--- researchengine/engine.py
+++ researchengine/engine.py
@@ -1,5 +1,18 @@
-from researchengine.logger import logger
+import json
 
+from pydantic import SecretStr
+
+from researchengine.llm_utils import get_openai_llm
+from dotenv import load_dotenv, find_dotenv
+from researchengine.logger import logger
+import os
+
+_ = load_dotenv(find_dotenv())
 
 def run(question):
     logger.info(f'{question} .. End.')
+
+
+    openai_key = SecretStr(os.environ['OPENAI_API_KEY'])
+    get_openai_llm(api_key=openai_key, model_name="gpt-4o-mini")
+
